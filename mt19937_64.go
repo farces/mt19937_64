814248abc678df946eb8e192bbf615b5079363d2
Adam McKay <amckhome@tpg.com.au>
2012-04-20 03:09:17 +1000

    go fmt

--- mt19937_64.go
+++ mt19937_64.go
@@ -37,19 +37,19 @@ const LOWER_MASK = 0x7FFFFFFF
 
 type MT19937_64 struct {
 	array [312]uint64 //state vector
-	index uint64 // array index
+	index uint64      // array index
 }
 
 func New() *MT19937_64 {
-	return &MT19937_64 {
-		index: N+1,
+	return &MT19937_64{
+		index: N + 1,
 	}
 }
 
 func (m *MT19937_64) Seed(seed int64) {
 	m.array[0] = uint64(seed)
-	for m.index = 1;m.index < N;m.index++ {
-		m.array[m.index] = (6364136223846793005 * (m.array[m.index-1] ^ (m.array[m.index-1] >> 62)) + m.index)
+	for m.index = 1; m.index < N; m.index++ {
+		m.array[m.index] = (6364136223846793005*(m.array[m.index-1]^(m.array[m.index-1]>>62)) + m.index)
 	}
 }
 
@@ -60,23 +60,23 @@ func (m *MT19937_64) Generate() int64 {
 func (m *MT19937_64) Int63() int64 {
 	var i int
 	var x uint64
-	mag01 := []uint64{0,MATRIX_A}
+	mag01 := []uint64{0, MATRIX_A}
 	if m.index >= N {
 		/* Initialize not called Initialize with a default */
 		if m.index == N+1 {
 			m.Seed(int64(5489))
 		}
 
-		for i = 0; i<N-M; i++ {
+		for i = 0; i < N-M; i++ {
 			x = (m.array[i] & UPPER_MASK) | (m.array[i+1] & LOWER_MASK)
-			m.array[i] = m.array[i+(M)] ^ (x>>1) ^ mag01[int(x&uint64(1))]
+			m.array[i] = m.array[i+(M)] ^ (x >> 1) ^ mag01[int(x&uint64(1))]
 		}
-		for ;i<N-1;i++ {
+		for ; i < N-1; i++ {
 			x = (m.array[i] & UPPER_MASK) | (m.array[i+1] & LOWER_MASK)
-			m.array[i] = m.array[i+(M-N)] ^ (x>>1) ^ mag01[int(x&uint64(1))]
+			m.array[i] = m.array[i+(M-N)] ^ (x >> 1) ^ mag01[int(x&uint64(1))]
 		}
 		x = (m.array[N-1] & UPPER_MASK) | (m.array[0] & LOWER_MASK)
-		m.array[N-1] = m.array[M-1] ^ (x>>1) ^ mag01[int(x&uint64(1))]
+		m.array[N-1] = m.array[M-1] ^ (x >> 1) ^ mag01[int(x&uint64(1))]
 		m.index = 0
 
 	}
